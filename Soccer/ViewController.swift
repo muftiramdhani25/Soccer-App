2dbb47bc4b67ee718e7f6759af762d1f678b117f
muhammad mufti ramdhani <muftiramdhani25@gmail.com>
2020-11-09 13:13:38 +0700

    Initial Commit

---
+++ Soccer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Soccer
+//
+//  Created by abdullah irianda on 09/11/20.
+//  Copyright © 2020 GROWDEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
